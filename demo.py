c889ffa7c507504e840c3378bca498c21ad34d48
Abhishek Kori <abhishekkori601@gmail.com>
2025-11-25 15:50:42 +0530

    data_validation

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from oil_retail import logger
 from oil_retail.pipline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from oil_retail.pipline.stage02_data_val import DataValidationTrainingPipeline
 
 STAGE_NAME = "data ingestion"
 try:
@@ -10,3 +11,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e    
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ oil_retail/pipline/stage02_data_val.py
@@ -0,0 +1,18 @@
+from oil_retail.config.configuration import ConfigurationManager
+from oil_retail.components.data_validation import DataValiadtion
+from oil_retail import logger
+
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+        
