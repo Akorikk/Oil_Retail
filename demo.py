24f03a6f9f1d96db3cf643636598d15560679c31
Abhishek Kori <abhishekkori601@gmail.com>
2025-11-25 15:15:15 +0530

    demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
 from oil_retail import logger
-logger.info("oil_assignment")
+from oil_retail.pipline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "data ingestion"
+try:
+    logger.info(f"stage {STAGE_NAME} started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"stage {STAGE_NAME} completed ")
+except Exception as e:
+    logger.exception(e)
+    raise e    
